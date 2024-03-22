bd3f2ee67b22eb92219334e70a9314d810f8c4ae
BohdanBoriak <lightraven72@gmail.com>
2024-03-22 13:32:12 +0200

    Added math game functionality

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+const (
+	totalPoints       = 50
+	pointsPerQuestion = 10
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Вітаємо у грі MATHREVENGE!")
+	time.Sleep(1 * time.Second)
+
+	for i := 5; i > 0; i-- {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+
+	myPoints := 0
+	start := time.Now()
+	for myPoints < totalPoints {
+		x, y := rand.Intn(100), rand.Intn(100)
+		fmt.Printf("%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+		if err != nil {
+			fmt.Println("Спробуй ще!")
+		} else {
+			if ansInt == x+y {
+				myPoints += pointsPerQuestion
+				points := totalPoints - myPoints
+				fmt.Println("Правильно, ти набрав", myPoints, "очок!")
+				fmt.Printf("Залишилось набрати %v!\n", points)
+			} else {
+				fmt.Println("Спробуй ще!")
+			}
+		}
+	}
+	end := time.Now()
+	timeSpent := end.Sub(start)
+
+	fmt.Printf("Молодчинка, впорався всього за %v!", timeSpent)
+	time.Sleep(5 * time.Second)
 }
